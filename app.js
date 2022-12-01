1f553e16a043c4387935807f21b23fc556996ccd
Roberto Drazewski <robertodrazewski@Robertos-MacBook-Pro.local>
2022-12-01 17:04:58 -0300

    portadas del sitio

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000
+app.listen(port);
+app.use(express.static('public'));
 
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./public/views/home.html'))
+});
 
+app.get('/registro',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./public/views/registro.html'))
+});
+
+app.get('/login',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./public/views/login.html'))
+});
+
+app.get('/producto',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./public/views/producto.html'))
+});
+
+app.get('/carrito',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./public/views/carrito.html'))
+});
 
