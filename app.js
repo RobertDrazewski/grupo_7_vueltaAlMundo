eab6110bc8948724d28ebff1fe87cd3cfd9735cb
magali09 <magalicristofori@hotmail.com>
2022-12-20 13:28:36 -0300

    cambio en producto

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000
+const port = 3000;
 app.listen(port, console.log("Corriendo en puerto 3000..."));
 app.use(express.static('public'));
 
@@ -18,7 +18,7 @@ app.get('/login',(req,res)=>{
 });
 
 app.get('/producto',(req,res)=>{
-    res.sendFile(path.join(__dirname,'/views/producto.html'))
+    res.sendFile(path.join(__dirname,'/views/productDetail.html'))
 });
 
 app.get('/carrito',(req,res)=>{
